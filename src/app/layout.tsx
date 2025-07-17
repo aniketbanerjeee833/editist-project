4a8239f7c279c2f6d9d8103448ec44a98bbd6df6
Aloy Entertainment <199nila@gmail.com>
2025-07-17 16:18:43 +0000

    how to change its name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Glitch Launch',
+  title: 'My Awesome App',
   description: 'Coming Soon',
 };
 
