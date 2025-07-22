464ee0f1c31fb64082b97ca9b903b13db57a88c1
Aloy Entertainment <199nila@gmail.com>
2025-07-22 18:08:56 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardDescription c

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -91,7 +91,7 @@ export default function ProjectsPage() {
                 </CardHeader>
                 <CardContent className="p-6">
                   <CardTitle className="text-2xl mb-2 text-center">{project.title}</CardTitle>
-                  <CardDescription className="text-muted-foreground mb-4">{project.description}</CardDescription>
+                  
                   <Button variant="outline">View Project</Button>
                 </CardContent>
               </Card>
