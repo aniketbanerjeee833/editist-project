a19b307fef18c2b1fa87c73f2c9683b778ac196d
Aloy Entertainment <199nila@gmail.com>
2025-07-22 18:14:03 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button>
    
    with the

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -93,7 +93,7 @@ export default function ProjectsPage() {
                 <CardContent className="p-6 flex flex-col flex-grow items-center">
                   <CardTitle className="text-2xl mb-4 text-center">{project.title}</CardTitle>
                   <div className="mt-auto">
-                    <Button>
+                    <Button className="transition-transform duration-300 ease-out hover:scale-105 hover:-translate-y-1">
                       View Project
                       <ChevronRight className="h-4 w-4" />
                     </Button>
